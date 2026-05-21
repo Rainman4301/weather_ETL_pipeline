dd15622c35ceb912e3cde3b1067b091d85630230
rainman4301 <liaoyuchieh6@gmail.com>
2026-05-21 10:25:51 +0930

    data modify

--- dbt/myproject/models/mart/weather_report.sql
+++ dbt/myproject/models/mart/weather_report.sql
@@ -1,22 +1,8 @@
-{{
-    config(
-        materialized='table',
-        unique_key='id',
-        schema='analytics'
-    )
-}}
+{{ config(materialized='table', unique_key='id') }}
 
 select
-    id,
-    city,
-    temperature,
-    weather_description,
-    wind_speed,
-    humidity,
-    pressure,
-    weather_time_local,
-    weather_time_utc,
-    is_forecast
-from
-{{ ref('stg_weather_data') }}
+    id, city, temperature, weather_description,
+    wind_speed, humidity, pressure,
+    weather_time_local, weather_time_utc, is_forecast
+from {{ ref('silver_weather_data') }}
 order by city, weather_time_local desc
